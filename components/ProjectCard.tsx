226d2bf153d3069251bbaafbac44b14264aed11d
OsmiiDev <76014006+OsmiiDev@users.noreply.github.com>
2022-08-29 08:06:57 -0500

    Added projects page

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,17 +1,22 @@
 import styles from "../styles/ProjectCard.module.css";
-import { SiNextdotjs, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si";
+import { SiGithub, SiHtml5, SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si";
 import { AiFillLock } from "react-icons/ai";
 
 import React from "react";
 import Image from "next/future/image";
+import { RiExternalLinkLine, RiInformationLine } from "react-icons/ri";
 export type ProjectCardProps = {
-  icon?: string;
-  link: string;
   title: string;
-  date: string;
   type: string;
+  date: string;
+
   description: string;
+
   tech: string[];
+
+  picture?: string;
+  link?: string;
+  github?: string;
 }
 
 const techIcons: {[key: string]: React.ReactNode} = {
@@ -20,9 +25,13 @@ const techIcons: {[key: string]: React.ReactNode} = {
   "Next.js": <SiNextdotjs size={25} color="#5865F2" className={styles.icon}/>,
   "Tailwindcss": <SiTailwindcss size={25} color="#5865F2" className={styles.icon}/>,
   "Typescript": <SiTypescript size={25} color="#5865F2" className={styles.icon}/>,
+  "HTML": <SiHtml5 size={25} color="#5865F2" className={styles.icon}/>,
+  "Javascript": <SiJavascript size={25} color="#5865F2" className={styles.icon}/>,
+  "Node.js": <SiNodedotjs size={25} color="#5865F2" className={styles.icon}/>,
+
 };
 
-export const ProjectCard: React.FC<ProjectCardProps> = ({ icon, link, title, date, type, description, tech }: ProjectCardProps) => {
+export const ProjectCard: React.FC<ProjectCardProps> = ({ title, type, date, description, tech, picture, link, github }: ProjectCardProps) => {
   return (
     <div className={styles.card}>
       <div className="w-full pt-4 h-13 flex items-center justify-between">
@@ -34,7 +43,23 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ icon, link, title, dat
       <p className={styles.secondaryInfo}> {date} • {type} </p>
       <div className={styles.divider}/>
       <p className="ml-5 m-3 float-left text-neutral-300 text-sm"> {description} </p>
-      {icon ? <Image src={icon} alt="project" width={1375} height={1286} className={styles.image} /> : ""}
+      {picture ? <Image src={picture} alt="project" width={1375} height={1286} className={styles.image} /> : ""}
+
+      {link ? (
+        <div className={styles.links}>
+          <a href={github} target="_blank" rel="noopener noreferrer" className={styles.link}>
+            <SiGithub size={20} color="#ffffff"/>
+          </a>
+
+          <a href={link} target="_blank" rel="noopener noreferrer" className={styles.link}>
+            <RiExternalLinkLine size={20}/> <span> View Project </span>
+          </a>
+
+          <a href={link} target="_blank" rel="noopener noreferrer" className={styles.link}>
+            <RiInformationLine size={20}/> <span> More </span>
+          </a>
+        </div>
+      ) : ""}
     </div>
   );
 };
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,26 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Projects.module.css";
 import { ProjectCard } from "../components/ProjectCard";
+import React from "react";
+import { RiHome2Line } from "react-icons/ri";
+import Link from "next/link";
 
-let touchStartX: number; let touchEndX: number;
-function touchStart(e) {
-  touchStartX = e.changedTouches[0].screenX;
-}
-
-function touchEnd(e) {
-  touchEndX = e.changedTouches[0].screenX;
-  checkDirection();
-}
-
-const THRESHOLD = 100;
-function checkDirection() {
-  if (touchStartX - touchEndX > THRESHOLD) {
-    alert("right");
-  }
-  if (touchStartX - touchEndX < -THRESHOLD) {
-    alert("left");
-  }
-} 
 
 const Projects: NextPage = () => {
   return (
@@ -30,51 +14,36 @@ const Projects: NextPage = () => {
         <title> Osmii </title>
       </Head>
 
+      <div className="float-left p-4 w-full">
+        <Link href="/" className="w-3 h-3">
+          <a className="flex items-center justify-center w-6 h-6 hover:text-neutral-500 transition-colors">
+            <RiHome2Line className="float-left" size={18}/>
+          </a>
+        </Link>
+      </div>
+
       <h1 className={styles.title}> Some projects I&#39;ve worked on </h1>
 
-      <div className={styles.projectCarouselContainer}
-        onTouchStart={touchStart} onTouchEnd={touchEnd}>
+      <div className={styles.projectCarousel} id="carousel">
+        <ProjectCard
+          title="My website"
+          type="Website"
+          date="2022-08-26"
+          description="A simple website built with Next.js, used to showcase projects and other stuff."
+          tech={["Next.js", "React", "Tailwindcss", "Typescript"]}
+          picture={"/Project_MyWebsite.png"}
+          github="https://github.com/OsmiiDev/About"
+          link="https://osmii.me"
+        />
 
-        <div className={styles.projectCarousel}>
-          <ProjectCard
-            title="My website"
-            type="Website"
-            date="2022-08-26"
-            description="A simple website built with Next.js, used to showcase projects and other stuff."
-            link="https://osmii.me"
-            tech={["Next.js", "React", "Tailwindcss", "Typescript"]}
-            icon={"/Project_MyWebsite.png"}
-          />
+        <ProjectCard
+          title="...And more"
+          type="Closed source projects"
+          date="2022-08-26"
+          description="There are more projects I've worked on that are not listed here, either due to me choosing to not share them or because they are private/closed source."
 
-          <ProjectCard
-            title="My website"
-            type="Website"
-            date="2022-08-26"
-            description="A simple website built with Next.js, used to showcase projects and other stuff."
-            link="https://osmii.me"
-            tech={["Next.js", "React", "Tailwindcss", "Typescript"]}
-            icon={"/Project_MyWebsite.png"}
-          />
-
-          <ProjectCard
-            title="My website"
-            type="Website"
-            date="2022-08-26"
-            description="A simple website built with Next.js, used to showcase projects and other stuff."
-            link="https://osmii.me"
-            tech={["Next.js", "React", "Tailwindcss", "Typescript"]}
-            icon={"/Project_MyWebsite.png"}
-          />
-
-          <ProjectCard
-            title="...And more"
-            type="Closed source projects"
-            date="2022-08-26"
-            description="There are more projects I've worked on that are not listed here, either due to me choosing to not share them or because they are private/closed source."
-            link="https://osmii.me"
-            tech={["Private"]}
-          />
-        </div>
+          tech={["Private"]}
+        />
       </div>
     </>
   );
